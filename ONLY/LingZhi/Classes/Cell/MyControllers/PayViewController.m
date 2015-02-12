6c563600ee0fb06f2b0827aa969cb4530a79479c
yy <yy__cf__ww@163.com>
2015-02-12 12:18:39 +0800

    add up

--- ONLY/LingZhi/Classes/Cell/MyControllers/PayViewController.m
+++ ONLY/LingZhi/Classes/Cell/MyControllers/PayViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "PayViewController.h"
-
+#import "UPViewController.h"
 @interface PayViewController ()
 
 @end
@@ -15,7 +15,7 @@
 @implementation PayViewController
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    self.navigationController.navigationBarHidden = NO;
+    self.navigationController.navigationBar.hidden = NO;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,15 +27,13 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+    银联充值
+ */
+- (IBAction)payAction:(UIButton *)sender {
+    UPViewController *vc = [[UPViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
-*/
+
 
 @end
